7a31dbbb26e5aa3a5dbef418678efd2a34a41d41
LgLinus <libuz94@gmail.com>
2016-03-11 14:20:23 +0100

    Camera front on

--- app/src/main/java/com/example/lenovo/smartcamera/MainActivity.java
+++ app/src/main/java/com/example/lenovo/smartcamera/MainActivity.java
@@ -1,32 +1,51 @@
 package com.example.lenovo.smartcamera;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
-    
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+
+public class MainActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    /* Camera variables */
+    // Used to display footage
+    CameraBridgeViewBase camera_view;
+
+    /* End of camera variables */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        
+
+        initiateComponents();
+
+
+    }
+
+    private void initiateComponents(){
+        camera_view = (CameraBridgeViewBase)findViewById(R.id.camera_view);
+
+        // Make the cameraview visible
+        camera_view.setVisibility(SurfaceView.VISIBLE);
+
+        camera_view.setCameraIndex(CameraBridgeViewBase.CAMERA_ID_FRONT);
+        // Set the listener to the implemented CvCameraViewListener
+        camera_view.setCvCameraViewListener(this);
     }
 
     @Override
@@ -50,4 +69,57 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    /**
+     * Called when the frame for the camera changes, here appropiate transformations should occur
+     * @param inputFrame
+     * @return
+     */
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame)
+    {
+        Mat res;
+        res = inputFrame.gray();
+
+        return res;
+    }
+
+
+    public void onCameraViewStarted(int width, int height)
+    {
+    }
+
+    public void onCameraViewStopped()
+    {
+    }
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        initOpenCV();
+    }
+
+    /**
+     * Init functions below
+     */
+    private void initOpenCV()
+    {
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_1_0, this, mLoaderCallback);
+    }
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this)
+    {
+        @Override
+        public void onManagerConnected(int status)
+        {
+            switch(status)
+            {
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    Log.d("init", "OpenCV loaded successfully");
+                    camera_view.enableView();
+                }
+            }
+        }
+    };
+
 }
